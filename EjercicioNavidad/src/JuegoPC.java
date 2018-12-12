42b269e3ae61aa0d9811a8b3e08edb5d56d386c2
MairoGP <mario_onlysk8@hotmail.com>
2018-12-12 21:25:14 +0100

    Introduccion de los modulos mostrarBiblioteca y AñadirJuego para el ejercicio de despues de navidad

--- EjercicioNavidad/src/JuegoPC.java
+++ EjercicioNavidad/src/JuegoPC.java
@@ -1,31 +1,92 @@
+import javax.swing.*;
+
 public class JuegoPC {
     private final String titulo;
     private final String descripcion;
-    private final String[] categoria;
-
-    private final String[] PLATAFORMAS = {
+    private String[] categoria;
+    public static  String[] PLATAFORMAS = {
             "PC",
             "PS4",
             "XBOX",
             "SWITCH",
             "MOVIL"
     };
-
     private  String[] plataformasDisponibles = new String[PLATAFORMAS.length];
-
-    private  String[] CATEGORIAS = {
-            "Acción",
+    public  static String[] CATEGORIAS = {
+            "Accion",
             "Aventuras",
             "First Person Shooter",
             "Puzles",
             "Narrativo",
+            "Fantasia",
+            "MOBA"
     };
 
-
-
-    public JuegoPC(String titulo, String descripcion, String[] categoria) {
+    JuegoPC(String titulo, String descripcion, String[] categorias) {
         this.titulo = titulo;
         this.descripcion = descripcion;
-        this.categoria = categoria;
+        this.categoria=categorias;
+        this.plataformasDisponibles[0] = "PC";
+        Steam.totalJuegosCreados++;
     }
+
+    public String mostrarJuego(){
+        String salida=" ";
+        salida+="Título:\t"+getTitulo()+"\n"+
+                "Descripción:\t"+getDescripcion()+"\n"+
+                "Categorias:\t"+getCategoria()+"\n"+
+                "Plataformas Disponibles:\t"+getPlataformasDisponibles();
+        return salida;
+    }
+
+    public void setCategoria(String categoriasDisponibles) {
+        boolean categoriaIncluida=false;
+        for (int i = 0; i < JuegoPC.CATEGORIAS.length; i++) {
+            if(categoriasDisponibles.equals(JuegoPC.CATEGORIAS[i])){
+               categoriaIncluida=true;
+            }
+        }
+        if(categoriaIncluida=false){
+            String respuesta = JOptionPane.showInputDialog(null,"La categoria a introducir no se encuentra guardada\n Desea guardar esta nueva?(Y/N)");
+            if(respuesta.charAt(0)=='Y'){
+                this.categoria[categoriasDisponibles.length()]=categoriasDisponibles;
+            } else {
+                JOptionPane.showMessageDialog(null,"No ha sido posible introducir la nueva categoria\n Asegurese de que la categoría esta definida");
+            }
+        }
+    }
+
+    public String getCategoria() {
+        String salida="";
+        for (int i = 0; i < this.categoria.length; i++) {
+            if(categoria[i]==null){
+                break;
+            } else {
+                salida+="\t"+this.categoria[i]+"\n";
+            }
+        }
+        return salida;
+    }
+
+    public String getPlataformasDisponibles() {
+        String salida="";
+        for (int i = 0; i < this.plataformasDisponibles.length; i++) {
+            if(plataformasDisponibles[i]==null){
+                break;
+            } else {
+                salida+="\t"+this.plataformasDisponibles[i]+"\n";
+            }
+        }
+        return salida;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+
 }
--- EjercicioNavidad/src/Steam.java
+++ EjercicioNavidad/src/Steam.java
@@ -1,26 +1,95 @@
 import javax.swing.*;
 
 public class Steam {
-    public static void main(String[] args) {
+    private static JuegoPC[] Biblioteca = new JuegoPC[10];
+    public static int totalJuegosCreados=0;
+    private static final String menu = "Bienvenido a la Biblioteca de Steam\n"+
+            "1. Mostrar Biblioteca\n"+
+            "2. Añadir Juego\n"+
+            "3. Borrar  Juego\n"+
+            "4. Buscar  Juego\n"+
+            "Introduce opción: ";
 
-        String menu = "Bienvenido a la Biblioteca de Steam\n"+
-                "1. Mostrar Biblioteca\n"+
-                "2. Añadir Juego\n"+
-                "3. Borrar u Juego\n"+
-                "4. Buscar un Juego\n"+
-                "5. Ordenar Alfabeticamente\n"+
-                "Introduce opción: ";
-        String respuesta="";
-        respuesta= JOptionPane.showInputDialog(null,menu);
-
-        switch (respuesta){
-            case 1: mostrarBiblioteca;
-            break;
+    public static void mostrarBiblioteca(){
+        for (int i = 0; i < Biblioteca.length; i++) {
+            if(Biblioteca[i]==null){
+                break;
+            } else {
+                JOptionPane.showMessageDialog(null,Biblioteca[i].mostrarJuego());
+            }
         }
-
-
-        System.out.println(respuesta);
-
-
     }
+
+    private static void añadirJuego() {
+        String titulo,descripcion;
+        String[] categorias = new String[JuegoPC.CATEGORIAS.length];
+
+        titulo = JOptionPane.showInputDialog(null,"Título: ");
+        descripcion = JOptionPane.showInputDialog(null,"Descripción: ");
+        JOptionPane.showMessageDialog(null,"Introduce una o varias categorias: \n");
+        int contador=0;
+        do {
+            categorias[contador] = JOptionPane.showInputDialog(null, "Categoria " + contador + ": ");
+            if (categorias[contador].charAt(0) == 0) {
+                continue;
+            } else {
+                boolean categoriaIncluida = false;
+                for (int i = 0; i < JuegoPC.CATEGORIAS.length; i++) {
+                    if (categorias[contador].equals(JuegoPC.CATEGORIAS[i])) {
+                        categoriaIncluida = true;
+                    }
+                }
+                if (categoriaIncluida == false && !categorias[contador].equals("0")) {
+                    String respuesta = JOptionPane.showInputDialog(null, "La categoria a introducir no se encuentra guardada\n Desea guardar esta nueva?(Y/N)");
+                    if (respuesta.charAt(0) == 'Y') {
+                        JuegoPC.CATEGORIAS[JuegoPC.CATEGORIAS.length - 1] = new String(categorias[contador]);
+                    } else if (respuesta.charAt(0) == 'N') {
+                        JOptionPane.showMessageDialog(null, "No ha sido posible introducir la nueva categoria\n Asegurese de que la categoría esta definida");
+                        break;
+                    }
+                }
+                contador++;
+            }
+        }while (categorias[contador - 1].charAt(0) != '0');
+            Biblioteca[totalJuegosCreados] = new JuegoPC(titulo, descripcion, categorias);
+    }
+
+
+    public static void main(String[] args) {
+        String[] categoriasRDR2 ={"Aventuras","Acción","Oeste"};
+        System.out.println(totalJuegosCreados);
+        Biblioteca[totalJuegosCreados] = new JuegoPC("Red Dead Redemption 2","Un joven vaquero con su banda intentan huir de la ley alejandose de la civilización",categoriasRDR2);
+        System.out.println(totalJuegosCreados);
+        String[] categoriasLOL = {"MOBA","Fantasia","Accion"};
+        Biblioteca[totalJuegosCreados] = new JuegoPC("League of Legends","Juego MOBA ultra popular sobre personajes dandose de ostias en la grieta",categoriasLOL);
+
+        int respuesta;
+        do {
+            respuesta = Integer.parseInt(JOptionPane.showInputDialog(null, menu));
+            switch (respuesta) {
+                case 1:
+                    mostrarBiblioteca();
+                    break;
+                case 2:
+                    añadirJuego();
+                    break;
+            }
+            System.out.println(respuesta);
+        }while(respuesta!=0);
+    }
+
 }
+
+
+/*public static int getTamanyoActualArray(String[] array){
+        int tamanyo=0;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i].equals(null)){
+                break;
+            } else{
+                tamanyo++;
+            }
+        }
+        return tamanyo;
+    }
+*/
